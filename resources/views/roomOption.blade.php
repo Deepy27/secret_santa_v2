a590888eeb71be38496dc270a77ef7ed3a50997f
Tamara Golobic <tamara.golobic@gmail.com>
2020-10-25 12:18:50 +0100

    Added navbar

--- resources/views/roomOption.blade.php
+++ resources/views/roomOption.blade.php
@@ -1,4 +1,29 @@
+<?php
+use App\User;
+$user = new User();
+?>
 @extends('layout.header')
+<nav class="navbar navbar-expand-lg navbar-dark">
+    <span class="navbar-text navbar-brand">
+    Dobrodešel/-la <?= $user->getUserName() ?>
+    </span>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="/">Home</a>
+            </li>
+            <li class="nav-item active">
+                <a class="nav-link" href="/room">Sobe</a>
+            </li>
+            <li class="nav-item active">
+                <a class="nav-link" href="/roomOption">Izbira sobe</a>
+            </li>
+        </ul>
+        <a href="/login">Vpis</a>
+        <a href="/register" class="mx-2">Registracija</a>
+        <a href="/logout">Izpis</a>
+    </div>
+</nav>
 <div class="form">
     <div>
         <h1>Izberi sobo</h1>
