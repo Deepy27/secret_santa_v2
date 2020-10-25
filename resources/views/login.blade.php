b8dc61d38dbe675b77042fbdd6a119179abb25d7
Tamara Golobic <tamara.golobic@gmail.com>
2020-10-25 11:21:10 +0100

    Changed background and button colors

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,19 +1,20 @@
 @extends('layout.header')
 <div class="container form">
-        <div class="col-4">
-            <h1>Vpiši se</h1>
-            <form method="post" action="login">
-                @csrf <!-- {{ csrf_field() }} -->
-                <div class="form-group">
-                    <input class="form-control" type="text" placeholder="Uporabniško Ime" name="username">
-                </div>
-                <div class="form-group">
-                    <input class="form-control" type="password" placeholder="Geslo" name="password">
-                </div>
-                <div class="form-group">
-                    <input class="btn btn-info btn-block" type="submit" value="Vpis">
-                </div>
-            </form>
-        </div>
+    <div class="col-4">
+        <h1>Vpiši se</h1>
+        <form method="post" action="login">
+        @csrf <!-- {{ csrf_field() }} -->
+            <div class="form-group">
+                <input class="form-control" type="text" placeholder="Uporabniško Ime" name="username">
+            </div>
+            <div class="form-group">
+                <input class="form-control" type="password" placeholder="Geslo" name="password">
+            </div>
+            <div class="form-group">
+                <input class="btn btn-outline-success btn-block" type="submit" value="Vpis">
+            </div>
+            <a href="/register" class="btn btn-outline-danger btn-block">Nisi registriran?</a>
+        </form>
+    </div>
 </div>
 @extends('layout.footer')
