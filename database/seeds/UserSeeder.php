15b070bccb587246381ef1dbf0b7f831acb3d443
Andraž Bajec <andraz.bajec27@gmail.com>
2020-10-25 10:01:47 +0100

    Added test user seed for testing

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\DB;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'Test user',
+            'username' => 'test',
+            'password' => Hash::make('test123')
+        ]);
+    }
+}
